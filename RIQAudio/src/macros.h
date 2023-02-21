2b25a914c66a93bf15a219f3b5232b32dd7dde1a
Braedon Lewis <braedonwilcox@gmail.com>
2023-02-21 04:27:30 -0500

    I am once again asking CMake to work

--- RIQAudio/src/macros.h
+++ RIQAudio/src/macros.h
@@ -1,5 +1,7 @@
-#if defined(__MACH__)
-#define DllExport
-#else
+#if defined(_MSC_VER)
 #define DllExport __declspec(dllexport)
+#elif defined(__GNUC__)
+#define DllExport __attribute__((visibility("default")))
+#else
+#define DllExport
 #endif
